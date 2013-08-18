3a9d1155356d65bfbc5761d4b88bc65e873ab59e
Raoul <raoul_dcunha@hotmail.com>
2013-08-18 20:54:06 +1200

    created a gmaps fragment, however it FC's, possibly due to the outdated version of Google Play Services installed on the emulator

--- src/com/kudos/openfoodfinder/MapActivity.java
+++ src/com/kudos/openfoodfinder/MapActivity.java
@@ -1,6 +1,7 @@
 package com.kudos.openfoodfinder;
 
 import android.os.Bundle;
+
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,12 +9,36 @@ import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
 
-public class MapActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MapActivity extends Activity {
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	private GoogleMap map;
+
+	//-----------------only works on honeycomb and newer! check for alternatives --------------
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg"));
+		Marker kiel = map.addMarker(new MarkerOptions().position(KIEL).title("Kiel").snippet("Kiel is cool").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+
+		// Move the camera instantly to hamburg with a zoom of 15.
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+		// Zoom in, animating the camera.
+		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
