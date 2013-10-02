fb269d155189440b448a85d7ba307a9590a0d6ee
Raoul <raoul_dcunha@hotmail.com>
2013-10-03 00:19:18 +1300

    fixed GPSLocation class

--- src/com/kudos/off/GPSLocation.java
+++ src/com/kudos/off/GPSLocation.java
@@ -1,24 +1,30 @@
 package com.kudos.off;
 
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+
 import android.content.Context;
 import android.location.Location;
-import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
 
-public class GPSLocation {
+public class GPSLocation implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener  {
 	
 	Location _location;
+	Context context;
+	private LocationClient mLocationClient;
 	
 	public GPSLocation(Context context)
 	{
-		LocationManager locManager;
-        locManager =(LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+		this.context=context;
+        mLocationClient = new LocationClient(context, this, this);
+        mLocationClient.connect();
 
-        _location = locManager
-                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (_location != null) {
-            double latitude = _location.getLatitude();
-            double longitude = _location.getLongitude();
-        }
+        
 	}
 	
 	//returns the Latitude of the current location
@@ -33,5 +39,36 @@ public class GPSLocation {
 		return _location != null?_location.getLongitude():0;
 		
 	}
+	
+
+	protected void updateWithNewLocation(Location location) {
+		_location = location;
+	}
+
+	 @Override
+	    public void onConnected(Bundle dataBundle) {
+	        // Display the connection status
+		 	_location = mLocationClient.getLastLocation();
+	        Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+
+	    }
+	    /*
+	     * Called by Location Services if the connection to the
+	     * location client drops because of an error.
+	     */
+	    @Override
+	    public void onDisconnected() {
+	        // Display the connection status
+	        Toast.makeText(context, "Disconnected. Please re-connect.",
+	                Toast.LENGTH_SHORT).show();
+	    }
+	    /*
+	     * Called by Location Services if the attempt to
+	     * Location Services fails.
+	     */
+	    @Override
+	    public void onConnectionFailed(ConnectionResult connectionResult) {
+	    }
+	
 
 }
